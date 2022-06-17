31623ddf6ff53743f71a907f821012869b8898fa
dysphere <keynesiane@gmail.com>
2022-06-17 16:05:34 -0700

    Change size when prompted by a button

--- javascript.js
+++ javascript.js
@@ -1,11 +1,35 @@
+function changeGridSize(size) {
 const container = document.querySelector(".container");
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < size; i++) {
     const containerRow = document.createElement("div");
     containerRow.classList.add("row");
     container.appendChild(containerRow);
-    for (let j = 0; j < 16; j++) {
+    for (let j = 0; j < size; j++) {
         const rowDiv = document.createElement("div");
         rowDiv.classList.add("rowDiv");
         containerRow.appendChild(rowDiv);
     }
 }
+const divBoxes = document.querySelectorAll(".rowDiv");
+divBoxes.forEach((rowDiv) => {
+    rowDiv.addEventListener("mouseenter", () => {
+        rowDiv.classList.add("colorChange");
+    });
+});
+}
+
+changeGridSize(16);
+
+const button = document.querySelector(".choice");
+button.addEventListener("click", () => {
+    let sizeString = prompt("How many squares do you want per side in your grid?");
+    let size = parseInt(sizeString);
+    const container = document.querySelector(".container");
+    while (container.firstChild) {
+        while (container.firstChild.firstChild) {
+            container.firstChild.removeChild(container.firstChild.firstChild);
+        }
+        container.removeChild(container.firstChild);
+    }
+    changeGridSize(size);
+});
